206e599486d1a3a881449d7fa73b5eec96626aa5
jgonzal3 <julio.gonzalez@ymail.com>
2018-06-06 23:35:42 +0100

    Create gauss.py

---
+++ utilities/gauss.py
@@ -0,0 +1,16 @@
+import random
+import math
+
+def GAUSS(SIG):
+	SUM=0
+	for j in range(1,7):
+		# THE NEXT STATEMENT PRODUCES A UNIF. DISTRIBUTED NUMBER FROM -0.5 and 0.5
+		IRAN=random.uniform(-0.5, 0.5)
+		SUM=SUM+IRAN
+		# In this case we have to multiply the resultant random variable by the square root of 2,
+	X=math.sqrt(2)*SUM*SIG
+	return (X)
+	
+def GAUSS_PY(SIG):
+	X=(random.uniform(-3.0,3.0))*SIG
+	return (X)
